b130a3ca29cdcf670a17d083a24f92ebc274f4ff
aliesmaeily68 <ali.61111@gmail.com>
2023-05-26 19:56:38 +0330

    create counter with class component react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import NavBar from "./Notfication/NavBar";
 import Counter from "./Counter";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Counter />
     </>
   );
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
+import "./Counter.css";
 
 export default class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      count: 10,
     };
     this.resetCounterHandler = this.resetCounterHandler.bind(this);
   }
+
   addCounterHandler() {
     console.log("addCounterHandler");
     this.setState((prev) => {
@@ -29,7 +31,9 @@ export default class Counter extends Component {
       <>
         <button onClick={this.resetCounterHandler}>reset</button>
         <button onClick={() => this.addCounterHandler()}>add</button>
-        {this.state.count}
+        <span className={`${this.state.count > 15 ? "hot" : "cold"}`}>
+          {this.state.count}°c
+        </span>
         <button onClick={this.minuseCounterHandler.bind(this)}>minues</button>
       </>
     );
